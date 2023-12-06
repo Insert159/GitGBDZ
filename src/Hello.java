6fc9bc929624543e62e3628291a89c77991d9adb
Insert159 <Insert159@mail.ru>
2023-12-06 20:13:03 +0700

    DONE

---
+++ src/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("1");
+        System.out.println("Гит");  // 2023.04.05
+    }
+}
